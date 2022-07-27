37c63accb0a785742382a1fce487a8ba07731901
higayasuo <higayasuo@gmail.com>
2022-07-27 03:51:19 +0000

    Implement getAccountAsset.ts

---
+++ front/src/lib/api/getAccountAssets.ts
@@ -0,0 +1,49 @@
+import algosdk, { Algodv2 } from 'algosdk';
+
+const getAccountAssets = async (client: Algodv2, address: string) => {
+  const accountInfo = await client
+    .accountInformation(address)
+    .setIntDecoding(algosdk.IntDecoding.BIGINT)
+    .do();
+
+  const algoBalance = accountInfo.amount as bigint;
+
+  const assets = await Promise.all(
+    accountInfo.assets.map(async (asset) => {
+      const id = Number(asset['asset-id']);
+      const { creator, frozen } = asset;
+      const amount = asset.amount as bigint;
+
+      const { params } = await client.getAssetByID(id).do();
+      const { name, 'unit-name': unitName, url, decimals } = params;
+
+      return {
+        id,
+        amount,
+        creator,
+        frozen,
+        name,
+        unitName,
+        url,
+        decimals,
+      };
+    })
+  );
+
+  assets.sort((a, b) => a.id - b.id);
+
+  assets.unshift({
+    id: 0,
+    amount: algoBalance,
+    creator: '',
+    frozen: false,
+    decimals: 6,
+    name: 'Algo',
+    unitName: 'Algo',
+    url: '',
+  });
+
+  return assets;
+};
+
+export default getAccountAssets;
